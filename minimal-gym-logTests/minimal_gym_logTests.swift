093029d25f0ffe283724ac038a727d92ea23df76
LeoCapontes <leo8capontes@hotmail.co.uk>
2026-01-03 17:43:12 +0000

    Add test for body weight exercise volume calculation
    Imported SwiftData and updated the asString test to specify unit preference. Added a new test stub for getBodyWeightExerciseVolume to prepare for testing volume calculations for body weight exercises.

--- minimal-gym-logTests/minimal_gym_logTests.swift
+++ minimal-gym-logTests/minimal_gym_logTests.swift
@@ -7,6 +7,7 @@
 
 import Testing
 import Foundation
+import SwiftData
 @testable import minimal_gym_log
 
 struct minimal_gym_logTests {
@@ -22,7 +23,20 @@ struct minimal_gym_logTests {
             date: Date()
         )
         
-        #expect(setBlock.asString() == "10kg for 12 reps x2\n10kg for 10 reps")
+        #expect(setBlock.asString(unitPreference: .kilogram) == "10kg for 12 reps x2\n10kg for 10 reps")
+    }
+    
+    @Test func getBodyWeightExerciseVolume() {
+        
+        let setBlock = SetBlock(
+            exercise: Exercise(name: "push ups", bodyPart: Exercise.BodyPart.chest),
+            sets: [
+                Set(reps: 12, weight: 10),
+                Set(reps: 12, weight: 10),
+                Set(reps: 10, weight: 10)
+            ],
+            date: Date()
+        )
     }
 
 }
